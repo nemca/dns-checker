213f27c8e6c9563049ee3b75d7e23a22f959a9f7
Mikhail Bruskov <mvbruskov@avito.ru>
2021-07-15 19:46:15 +0300

    init

---
+++ README.md
@@ -0,0 +1,44 @@
+# dns-checker
+
+The tool for diagnostics DNS.
+
+#### Usage
+```
+$ dns-checker --help
+Usage of dns-checker:
+  -c, --count int             number of requests to send. Default is to run until ^C
+  -i, --interval duration     interval between requests (default 100ms)
+  -n, --name string           resource record name
+  -s, --nameservers strings   comma separated nameservers names with port (default [10.0.0.1:53])
+  -r, --no-recursion          disable recursion desired flag
+  -e, --only-errors           show only errors
+  -q, --query-type string     query type to use (A, AAAA, SOA, CNAME...) (default "A")
+  -t, --timeout duration      query timeout (default 2s)
+```
+
+#### Example
+
+Query DNS record mixanemca.ru with type A for two DNS nameservers (1.1.1.1 and 8.8.8.8) two times.
+
+```
+$ dns-checker --name mixanemca.ru. --query-type A --nameservers "1.1.1.1:53,8.8.8.8:53" --count 2
+2021-07-15 19:43:38.806987 +0300 MSK m=+0.174620341
+Nameserver: 8.8.8.8:53
+mixanemca.ru.	3599	IN	A	134.209.90.80
+
+2021-07-15 19:43:39.415163 +0300 MSK m=+0.782800789
+Nameserver: 1.1.1.1:53
+mixanemca.ru.	3600	IN	A	134.209.90.80
+
+2021-07-15 19:43:39.419918 +0300 MSK m=+0.787556393
+Nameserver: 1.1.1.1:53
+mixanemca.ru.	3597	IN	A	134.209.90.80
+
+2021-07-15 19:43:40.837345 +0300 MSK m=+2.204993198
+Nameserver: 8.8.8.8:53
+exchange failed: read udp 192.168.44.99:51871->8.8.8.8:53: i/o timeout (rtt: 2.000037788s)
+
+--- mixanemca.ru. dns check statistics ---
+1 error (25.00%) and 3 success (75.00%) for 2 nameservers.
+round-trip min/avg/max = 4.638/251.414/678.894 ms
+```
---
+++ main.go
@@ -0,0 +1,164 @@
+/*
+Copyright © 2021 Michael Bruskov <mixanemca@yandex.ru>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"fortio.org/fortio/stats"
+	"github.com/miekg/dns"
+	"github.com/spf13/pflag"
+	"golang.org/x/sync/errgroup"
+)
+
+// flags
+var (
+	name        string
+	nameservers []string
+	queryType   string
+	noRecursion bool
+	onlyError   bool
+	timeout     time.Duration
+	interval    time.Duration
+	count       int
+	// version string
+)
+
+// stats
+var (
+	errorCount   int
+	successCount int
+)
+
+func init() {
+	pflag.StringVarP(&name, "name", "n", "", "resource record name")
+	pflag.StringSliceVarP(&nameservers, "nameservers", "s", []string{"10.0.0.1:53"}, "comma separated nameservers names with port")
+	pflag.StringVarP(&queryType, "query-type", "q", "A", "query type to use (A, AAAA, SOA, CNAME...)")
+	pflag.BoolVarP(&noRecursion, "no-recursion", "r", false, "disable recursion desired flag")
+	pflag.BoolVarP(&onlyError, "only-errors", "e", false, "show only errors")
+	pflag.DurationVarP(&timeout, "timeout", "t", 2*time.Second, "query timeout")
+	pflag.DurationVarP(&interval, "interval", "i", 100*time.Millisecond, "interval between requests")
+	pflag.IntVarP(&count, "count", "c", 0, "number of requests to send. Default is to run until ^C")
+}
+
+func main() {
+	pflag.Parse()
+
+	if len(name) == 0 || len(nameservers) < 1 {
+		pflag.Usage()
+		os.Exit(1)
+	}
+
+	qt, exists := dns.StringToType[strings.ToUpper(queryType)]
+	if !exists {
+		fmt.Printf("Invalid query type %q\n", queryType)
+		os.Exit(1)
+	}
+
+	// Create DNS message
+	msg := new(dns.Msg)
+	msg.Id = dns.Id()
+	msg.SetQuestion(name, qt)
+	msg.RecursionDesired = !noRecursion
+
+	// Channel for terminating by signal
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	// Channel for stop goroutings
+	done := make(chan int)
+
+	// Stats
+	statHistogram := stats.NewHistogram(0, 0.1)
+
+	go func(done chan int) {
+		<-interrupt
+		close(done)
+	}(done)
+
+	g := new(errgroup.Group)
+
+	for _, nserver := range nameservers {
+		ns := nserver // https://golang.org/doc/faq#closures_and_goroutines
+		g.Go(func() error {
+			pinger(ns, msg, interval, statHistogram, done)
+			return nil
+		})
+	}
+	// Wait errgroup
+	err := g.Wait()
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+	// Show statistics
+	showStat(statHistogram)
+}
+
+func pinger(ns string, m *dns.Msg, t time.Duration, statHistogram *stats.Histogram, done chan int) {
+	c := dns.Client{
+		Timeout: timeout,
+	}
+	ticker := time.NewTicker(t)
+	for i := 1; count <= 0 || i <= count; i++ {
+		select {
+		case <-done:
+			ticker.Stop()
+			return
+		case <-ticker.C:
+			var buf bytes.Buffer
+			res, rtt, err := c.Exchange(m, ns)
+			if err != nil {
+				fmt.Fprintf(&buf, "%v\n", time.Now())
+				fmt.Fprintf(&buf, "Nameserver: %s\n", ns)
+				fmt.Fprintf(&buf, "exchange failed: %v (rtt: %v)\n", err, rtt)
+				fmt.Println(buf.String())
+				errorCount++
+				continue
+			}
+			rttMS := 1000. * rtt.Seconds()
+			statHistogram.Record(rttMS)
+			successCount++
+			if !onlyError {
+				fmt.Fprintf(&buf, "%v\n", time.Now())
+				fmt.Fprintf(&buf, "Nameserver: %s\n", ns)
+				for _, a := range res.Answer {
+					fmt.Fprintln(&buf, a.String())
+				}
+				fmt.Println(buf.String())
+			}
+		}
+	}
+}
+
+func showStat(statHistogram *stats.Histogram) {
+	errorPerc := fmt.Sprintf("%.02f%%", 100.*float64(errorCount)/float64(errorCount+successCount))
+	successPerc := fmt.Sprintf("%.02f%%", 100.*float64(successCount)/float64(errorCount+successCount))
+	plural := "s" // 0 errors 1 error 2 errors...
+	if errorCount == 1 {
+		plural = ""
+	}
+
+	fmt.Printf("--- %s dns check statistics ---\n", name)
+	fmt.Printf("%d error%s (%s) and %d success (%s) for %d nameservers.\n", errorCount, plural, errorPerc, successCount, successPerc, len(nameservers))
+	res := statHistogram.Export()
+	fmt.Printf("round-trip min/avg/max = %.3f/%.3f/%.3f ms\n", res.Min, res.Avg, res.Max)
+}
